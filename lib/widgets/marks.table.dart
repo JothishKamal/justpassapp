98cce91c0337ac3d4d5b6f76796f18fb678518d0
LakshBharani <laksh.bharani@gmail.com>
2024-06-08 23:03:49 +0530

    refactored

--- lib/widgets/marks.table.dart
+++ lib/widgets/marks.table.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:justpassapp/consts.dart';
+import 'package:justpassapp/cubit/subject_data.cubit.dart';
 
 Widget marksTable() {
+  final subjectdataCubit = SubjectDataCubit();
   return Container(
     decoration: BoxDecoration(
       color: fieldbg,
@@ -9,61 +11,44 @@ Widget marksTable() {
     ),
     padding: const EdgeInsets.all(10),
     child: SingleChildScrollView(
-      scrollDirection: Axis.horizontal,
-      child: DataTable(
-        columns: const <DataColumn>[
-          DataColumn(
-            label: Text(
-              'Subject',
-              style: TextStyle(fontWeight: FontWeight.bold),
+        scrollDirection: Axis.horizontal,
+        child: DataTable(
+          columns: const <DataColumn>[
+            DataColumn(
+              label: Text(
+                'Subject',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-          DataColumn(
-            label: Text(
-              'CAT - 1',
-              style: TextStyle(fontWeight: FontWeight.bold),
+            DataColumn(
+              label: Text(
+                'CAT - 1',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-          DataColumn(
-            label: Text(
-              'CAT - 2',
-              style: TextStyle(fontWeight: FontWeight.bold),
+            DataColumn(
+              label: Text(
+                'CAT - 2',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-          DataColumn(
-            label: Text(
-              'FAT',
-              style: TextStyle(fontWeight: FontWeight.bold),
+            DataColumn(
+              label: Text(
+                'FAT',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
-        ],
-        rows: const <DataRow>[
-          DataRow(
-            cells: <DataCell>[
-              DataCell(Text('Engineering Physics')),
-              DataCell(Text('80')),
-              DataCell(Text('85')),
-              DataCell(Text('90')),
-            ],
-          ),
-          DataRow(
-            cells: <DataCell>[
-              DataCell(Text('Engineering Chemistry')),
-              DataCell(Text('75')),
-              DataCell(Text('80')),
-              DataCell(Text('85')),
-            ],
-          ),
-          DataRow(
-            cells: <DataCell>[
-              DataCell(Text('Mathematics')),
-              DataCell(Text('70')),
-              DataCell(Text('75')),
-              DataCell(Text('80')),
-            ],
-          ),
-        ],
-      ),
-    ),
+          ],
+          rows: subjectdataCubit.state['subjects'].map<DataRow>((subject) {
+            return DataRow(
+              cells: <DataCell>[
+                DataCell(Text(subject)),
+                const DataCell(const Text('80')),
+                const DataCell(Text('85')),
+                const DataCell(Text('90')),
+              ],
+            );
+          }).toList(),
+        )),
   );
 }
