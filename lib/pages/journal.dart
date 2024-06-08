84b5fc3033ca9832bbe1478746cd7ce481b3bdbf
LakshBharani <laksh.bharani@gmail.com>
2024-06-08 18:53:44 +0530

    Journal page updated

--- lib/pages/journal.dart
+++ lib/pages/journal.dart
@@ -9,7 +9,44 @@ class JournalPage extends StatefulWidget {
   State<JournalPage> createState() => _JournalPageState();
 }
 
+List entryData = [
+  {
+    'date': '7th June 2024',
+    'title':
+        'A Heartfelt Day: Celebrating my birthday in the midst of exam season',
+    'content':
+        'Today was a whirlwind of emotions as I celebrated my birthday amidst the stress of exam season. Despite the looming deadlines and piles of notes, my friends and piles of notes, my friends ...',
+  },
+  {
+    'date': '5th June 2024',
+    'title': 'A Day of Reflection: My journey to self-discovery',
+    'content':
+        'Today was a day of reflection as I embarked on a journey of self-discovery. I spent the day exploring new hobbies and interests, and it was a refreshing change of pace from my usual routine ...',
+  },
+  {
+    'date': '3rd June 2024',
+    'title': 'A Day of Adventure: Exploring the great outdoors',
+    'content':
+        'Today was a day of adventure as I explored the great outdoors. I hiked through the mountains, swam in the crystal-clear waters of the lake, and basked in the warm glow of the sun ...',
+  },
+  {
+    'date': '1st June 2024',
+    'title': 'A Day of Creativity: Unleashing my inner artist',
+    'content':
+        'Today was a day of creativity as I unleashed my inner artist. I spent the day painting, drawing, and creating, and it was a cathartic experience that allowed me to express myself in new ways ...',
+  },
+  {
+    'date': '30th May 2024',
+    'title': 'A Day of Relaxation: Taking time to unwind',
+    'content':
+        'Today was a day of relaxation as I took time to unwind. I indulged in a long bath, read a good book, and enjoyed a leisurely walk in the park. It was a much-needed break from the hustle and bustle of daily life ...',
+  },
+];
+
 class _JournalPageState extends State<JournalPage> {
+  List activeEntries = entryData;
+  final searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -58,14 +95,28 @@ class _JournalPageState extends State<JournalPage> {
             const SizedBox(height: 10),
             Row(
               children: [
-                const Expanded(
+                Expanded(
                   child: SizedBox(
                     height: 40,
                     child: TextField(
-                      style: TextStyle(
+                      controller: searchController,
+                      onChanged: (value) {
+                        setState(() {
+                          activeEntries = entryData
+                              .where((entry) =>
+                                  entry['title']
+                                      .toLowerCase()
+                                      .contains(value.toLowerCase()) ||
+                                  entry['content']
+                                      .toLowerCase()
+                                      .contains(value.toLowerCase()))
+                              .toList();
+                        });
+                      },
+                      style: const TextStyle(
                         fontSize: 15,
                       ),
-                      decoration: InputDecoration(
+                      decoration: const InputDecoration(
                         isCollapsed: true,
                         contentPadding: EdgeInsets.all(9),
                         isDense: true,
@@ -89,34 +140,55 @@ class _JournalPageState extends State<JournalPage> {
                     ),
                   ),
                 ),
-                IconButton(
-                  // Add an IconButton to the right of the TextField
-                  icon: const Icon(Icons.tune),
-                  color: Colors.white,
-                  onPressed: () {
-                    // Add your action here
-                  },
-                ),
               ],
             ),
             const SizedBox(height: 20),
             Expanded(
-                child: ListView.separated(
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return Container(
-                  height: 100,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      color: const Color(0xFFD9D9D9)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: entry(),
-                  ),
-                );
-              },
-              separatorBuilder: (context, index) => const SizedBox(height: 10),
-            )),
+              child: ListView.separated(
+                itemCount: activeEntries.length,
+                separatorBuilder: (context, index) =>
+                    const SizedBox(height: 20),
+                itemBuilder: (context, index) {
+                  return Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20, vertical: 15),
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(255, 217, 217, 217),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          activeEntries[index]['date'],
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          activeEntries[index]['title'],
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        Text(
+                          activeEntries[index]['content'],
+                          style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
           ],
         ),
       )),
@@ -130,14 +202,19 @@ Widget salutation(String name) {
       const Text(
         'Hi',
         style: TextStyle(
-            color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),
+          color: Colors.white,
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          fontFamily: 'Newsreader',
+        ),
       ),
       const SizedBox(width: 5),
       Text(
-        "${name},",
+        "$name,",
         style: const TextStyle(
           color: Colors.white,
           fontSize: 30,
+          fontFamily: 'Newsreader',
         ),
       ),
     ],
